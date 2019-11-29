02cee695f8b8aa776aa59ba375ecba73e6e3d857
JoonSeongPark <rytt@yonsei.ac.kr>
2019-11-29 20:43:42 +0900

    font updated

--- App.js
+++ App.js
@@ -1,9 +1,34 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+import { enableScreens } from "react-native-screens";
 
 import WineNavigator from "./navigation/WineNavigator";
 
+enableScreens;
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'vagrounded': require("./assets/fonts/vagrounded-regular.ttf"),
+    "vagrounded-bold": require("./assets/fonts/vagrounded-bold.ttf"),
+    "kakao": require("./assets/fonts/KakaoRegular.ttf")
+  });
+};
+
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={err => console.log(err)}
+      />
+    );
+  }
+
   return <WineNavigator />;
 }
 
